2ed514027450379f2d822257f2080bc31a56f1f1
Dave Hall <dave@etianen.com>
2013-07-19 00:38:27 +0100

    Making tests more concise

--- dj_authtokens/tests.py
+++ dj_authtokens/tests.py
@@ -40,252 +40,213 @@ class TestAuthTokens(TestCase):
 
     # Valid token tests.
 
-    def assertScopeCorrect(self, scope, parent_scope, expected):
+    def assertScope(self, scope, parent_scope, expected):
         token = tokens.generate(scope)
         self.assertEqual(tokens.validate(token, parent_scope), expected)
 
+    def assertScopeValid(self, scope, parent_scope):
+        return self.assertScope(scope, parent_scope, True)
+
+    def assertScopeInvalid(self, scope, parent_scope):
+        return self.assertScope(scope, parent_scope, False)
+
     def testScopePermissionGrants(self):
         # Asking for no permissions.
-        self.assertScopeCorrect(
+        self.assertScopeValid(
             scope.access_all(),
             scope.access_all(),
-            True,
         )
-        self.assertScopeCorrect(
+        self.assertScopeValid(
             scope.access_all(),
             scope.access_all("read"),
-            True,
         )
-        self.assertScopeCorrect(
+        self.assertScopeValid(
             scope.access_all(),
             scope.access_all("read", "write"),
-            True,
         )
         # Asking for read permissions.
-        self.assertScopeCorrect(
+        self.assertScopeInvalid(
             scope.access_all("read"),
             scope.access_all(),
-            False,
         )
-        self.assertScopeCorrect(
+        self.assertScopeValid(
             scope.access_all("read"),
             scope.access_all("read"),
-            True,
         )
-        self.assertScopeCorrect(
+        self.assertScopeValid(
             scope.access_all("read"),
             scope.access_all("read", "write"),
-            True,
         )
         # Asking for read and write permissions.
-        self.assertScopeCorrect(
+        self.assertScopeInvalid(
             scope.access_all("read", "write"),
             scope.access_all(),
-            False,
         )
-        self.assertScopeCorrect(
+        self.assertScopeInvalid(
             scope.access_all("read", "write"),
             scope.access_all("read"),
-            False,
         )
-        self.assertScopeCorrect(
+        self.assertScopeValid(
             scope.access_all("read", "write"),
             scope.access_all("read", "write"),
-            True,
         )
 
     def testScopeModelGrants(self):
         # Ask for no access.
-        self.assertScopeCorrect(
+        self.assertScopeValid(
             (),
             scope.access_all("read"),
-            True,
         )
-        self.assertScopeCorrect(
+        self.assertScopeValid(
             (),
             scope.access_app("dj_authtokens", "read"),
-            True,
         )
-        self.assertScopeCorrect(
+        self.assertScopeValid(
             (),
             scope.access_model(TestModel, "read"),
-            True,
         )
-        self.assertScopeCorrect(
+        self.assertScopeValid(
             (),
             scope.access_obj(self.obj, "read"),
-            True,
         )
-        self.assertScopeCorrect(
+        self.assertScopeValid(
             (),
             (),
-            True,
         )
         # Ask for access, but no permissions
-        self.assertScopeCorrect(
+        self.assertScopeValid(
             scope.access_all(),
             scope.access_all("read"),
-            True,
         )
-        self.assertScopeCorrect(
+        self.assertScopeValid(
             scope.access_all(),
             scope.access_app("dj_authtokens", "read"),
-            True,
         )
-        self.assertScopeCorrect(
+        self.assertScopeValid(
             scope.access_all(),
             scope.access_model(TestModel, "read"),
-            True,
         )
-        self.assertScopeCorrect(
+        self.assertScopeValid(
             scope.access_all(),
             scope.access_obj(self.obj, "read"),
-            True,
         )
-        self.assertScopeCorrect(
+        self.assertScopeValid(
             scope.access_all(),
             (),
-            True,
         )
         # Ask for obj access.
-        self.assertScopeCorrect(
+        self.assertScopeValid(
             scope.access_obj(self.obj, "read"),
             scope.access_all("read"),
-            True,
         )
-        self.assertScopeCorrect(
+        self.assertScopeValid(
             scope.access_obj(self.obj, "read"),
             scope.access_app("dj_authtokens", "read"),
-            True,
         )
-        self.assertScopeCorrect(
+        self.assertScopeValid(
             scope.access_obj(self.obj, "read"),
             scope.access_model(TestModel, "read"),
-            True,
         )
-        self.assertScopeCorrect(
+        self.assertScopeValid(
             scope.access_obj(self.obj, "read"),
             scope.access_obj(self.obj, "read"),
-            True,
         )
-        self.assertScopeCorrect(
+        self.assertScopeInvalid(
             scope.access_obj(self.obj, "read"),
             (),
-            False,
         )
         # Ask for model access.
-        self.assertScopeCorrect(
+        self.assertScopeValid(
             scope.access_model(TestModel, "read"),
             scope.access_all("read"),
-            True,
         )
-        self.assertScopeCorrect(
+        self.assertScopeValid(
             scope.access_model(TestModel, "read"),
             scope.access_app("dj_authtokens", "read"),
-            True,
         )
-        self.assertScopeCorrect(
+        self.assertScopeValid(
             scope.access_model(TestModel, "read"),
             scope.access_model(TestModel, "read"),
-            True,
         )
-        self.assertScopeCorrect(
+        self.assertScopeInvalid(
             scope.access_model(TestModel, "read"),
             scope.access_obj(self.obj, "read"),
-            False,
         )
-        self.assertScopeCorrect(
+        self.assertScopeInvalid(
             scope.access_model(TestModel, "read"),
             (),
-            False,
         )
         # Ask for app access.
-        self.assertScopeCorrect(
+        self.assertScopeValid(
             scope.access_app("dj_authtokens", "read"),
             scope.access_all("read"),
-            True,
         )
-        self.assertScopeCorrect(
+        self.assertScopeValid(
             scope.access_app("dj_authtokens", "read"),
             scope.access_app("dj_authtokens", "read"),
-            True,
         )
-        self.assertScopeCorrect(
+        self.assertScopeInvalid(
             scope.access_app("dj_authtokens", "read"),
             scope.access_model(TestModel, "read"),
-            False,
         )
-        self.assertScopeCorrect(
+        self.assertScopeInvalid(
             scope.access_app("dj_authtokens", "read"),
             scope.access_obj(self.obj, "read"),
-            False,
         )
-        self.assertScopeCorrect(
+        self.assertScopeInvalid(
             scope.access_app("dj_authtokens", "read"),
             (),
-            False,
         )
         # Ask for global access.
-        self.assertScopeCorrect(
+        self.assertScopeValid(
             scope.access_all("read"),
             scope.access_all("read"),
-            True,
         )
-        self.assertScopeCorrect(
+        self.assertScopeInvalid(
             scope.access_all("read"),
             scope.access_app("dj_authtokens", "read"),
-            False,
         )
-        self.assertScopeCorrect(
+        self.assertScopeInvalid(
             scope.access_all("read"),
             scope.access_model(TestModel, "read"),
-            False,
         )
-        self.assertScopeCorrect(
+        self.assertScopeInvalid(
             scope.access_all("read"),
             scope.access_obj(self.obj, "read"),
-            False,
         )
-        self.assertScopeCorrect(
+        self.assertScopeInvalid(
             scope.access_all("read"),
             (),
-            False,
         )
 
     def testKitchenSink(self):
         # Access specific models using a global read token.
-        self.assertScopeCorrect(
+        self.assertScopeValid(
             scope.access_obj(self.obj, "read") + scope.access_obj(self.obj2, "read"),
             scope.access_all("read"),
-            True,
         )
         # Then fail it by asking for a new permission.
-        self.assertScopeCorrect(
+        self.assertScopeInvalid(
             scope.access_obj(self.obj, "read", "write") + scope.access_obj(self.obj2, "read"),
             scope.access_all("read"),
-            False,
         )
         # Access specific objects using a specific read and write token.
-        self.assertScopeCorrect(
+        self.assertScopeValid(
             scope.access_obj(self.obj, "read", "write") + scope.access_obj(self.obj2, "read", "write"),
             scope.access_model(TestModel, "read", "write") + scope.access_model(TestModel2, "read", "write"),
-            True,
         )
         # Then fail it because access wasn't granted to the second model.
-        self.assertScopeCorrect(
+        self.assertScopeInvalid(
             scope.access_obj(self.obj, "read", "write") + scope.access_obj(self.obj2, "read", "write"),
             scope.access_model(TestModel, "read", "write"),
-            False,
         )
         # Then give it back with a token for the whole app.
-        self.assertScopeCorrect(
+        self.assertScopeValid(
             scope.access_obj(self.obj, "read", "write") + scope.access_obj(self.obj2, "read", "write"),
             scope.access_model(TestModel, "read", "write") + scope.access_app("dj_authtokens", "read", "write"),
-            True,
         )
         # Finally, give read access to everything, write access to a specific model, and it should work.
-        self.assertScopeCorrect(
+        self.assertScopeValid(
             scope.access_obj(self.obj, "read", "write"),
             scope.access_model(self.obj, "write") + scope.access_all("read"),
-            True,
         )
